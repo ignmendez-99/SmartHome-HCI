aa9f3b5e5759a7dd7afe4eff56dcf4973af33670
Juan Gabriel Griggio <juangriggio@gmail.com>
2020-05-22 19:55:09 -0300

    AMAZING, UNBELIEVABLE, BREATH-TAKING CHANGES AND IMPROVEMENTS

--- src/components/device.vue
+++ src/components/device.vue
@@ -1,8 +1,8 @@
 <template>
     <v-col cols="3">
         <v-card class="mx-auto">
-            <v-img class="white--text align-end" height="120px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
-                <v-card-title>{{name}}</v-card-title>
+            <v-img class="white--text align-end" height="80px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                <v-card-title>{{deviceName}}</v-card-title>
             </v-img>
             <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
             <v-card-text class="text--primary">
@@ -10,12 +10,8 @@
                 <div>Whitsunday Island, Whitsunday Islands</div>
             </v-card-text>
             <v-card-actions>
-                <v-btn color="orange" text>
-                    Share
-                </v-btn>
-                <v-btn color="orange" text>
-                    Explore
-                </v-btn>
+                <v-btn class="blue--text" text>ON</v-btn>
+                <v-btn class="blue--text" text>OFF</v-btn>
             </v-card-actions>
         </v-card>
     </v-col>
@@ -24,7 +20,7 @@
 <script>
 export default {
     props: {
-        name: String
+        deviceName: String
     },
     data() {
         return{
--- src/components/room.vue
+++ src/components/room.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row class="mx-4">
-        <p id="npnm" class="subtitle-1">{{name}}</p>
+        <p id="npnm" class="subtitle-1">{{roomName}}</p>
         <v-spacer></v-spacer>
         <v-btn small @click="editPressed" v-show="expand">{{editingText}}</v-btn>
         <v-btn small @click="expandPressed">
@@ -12,30 +12,8 @@
         <v-col cols="12" class="shrink">
             <v-expand-transition>
                 <v-row v-show="expand" class="mx-4" align="center">
-                    <device v-for="device in myDevices" :key="device" :name="device[aux]"/>
-
-                    <v-col cols="3" v-show="editing">
-                        <v-col cols="2"/>
-                        <v-col>
-                            <v-card @click="createDevice">
-                                <v-container>
-                                    <v-row align="center">
-                                        <!-- <v-col cols="2"/> -->
-                                        <v-col align="center" color="blue lighten-3" class="pt-0 mt-0">
-                                            <div class="blue darken-1 pa-0 ma-0">
-                                                <v-icon size="60" class="pa-1 ma-1 white--text">mdi-plus</v-icon>
-                                            </div>
-                                        </v-col>
-                                        <!-- <v-col cols="2"/> -->
-                                        <v-col cols="12" align="center" class="pa-0 ma-0">
-                                            <p class="ma-0 pa-0 title">Add</p>
-                                        </v-col>
-                                    </v-row>
-                                </v-container>
-                            </v-card>
-                        </v-col>
-                        <v-col cols="2"/>                        
-                    </v-col>
+                    <device v-for="device in $deviceStore.data.devicesByRoom.get(roomId)" :key="device" :deviceName="device[nameString]"/>
+                    <addDevice :roomId="roomId"/>
                 </v-row>
             </v-expand-transition>
         </v-col>
@@ -44,25 +22,24 @@
 
 <script>
 import device from "../components/device.vue"
+import addDevice from "../components/addDevice.vue"
 
 export default {
     components: {
-        'device': device
+        'device': device,
+        'addDevice': addDevice
     },
     props: {
-        name: String,
-        devices: Array,
-        id: String
+        roomName: String,
+        roomId: String,
     },
     data() {
         return{
             editing: false,
-            aux: "name",
+            nameString: "name",
             editingText: "Edit",
             expand: false,
-            arrow: "mdi-arrow-down",
-            myDevices: [],
-            genericUrl: 'http://127.0.0.1:8081/api/'
+            arrow: "mdi-arrow-down"
         }
     },
     methods: {
@@ -79,47 +56,7 @@ export default {
                 this.arrow = "mdi-arrow-up"
             else
                 this.arrow = "mdi-arrow-down"
-        },
-        parseDevices(device) {
-            if(device["room"]["name"] === this.name)
-                this.myDevices.push(device);
-        },
-        createDevice() {
-            let deviceId;
-
-            this.axios.post(this.genericUrl + 'devices', {
-                "type": {
-                    "id": "go46xmbqeomjrsjr"
-                },
-                "name": "DEVICE 1",
-                "meta": {}
-            })
-            .then( (response) => {
-                deviceId = response.data.result.id;
-                this.axios.post(this.genericUrl + "rooms/" + this.id + "/devices/" + deviceId, {})
-                .then( () => {
-                    console.log("LINKED ROOM WITH DEVICE");
-                    this.axios.get(this.genericUrl + "devices/" + deviceId, {})
-                    .then( (response) => {
-                        this.myDevices.push(response.data.result);
-                        console.log("SUCCESS");
-                    })
-                    .catch( () => {
-                        console.log("FAILED TO GET THE NEW DEVICE");
-                    })
-                })
-                .catch( () => {
-                    console.log("FAILED TO LINK DEVICE WITH ROOM");
-                })
-            })
-            .catch( (response) =>{
-                console.log("FAILED TO CREATE DEVICE : " + response);
-            })
         }
-    },
-    mounted() {
-        this.devices.forEach(this.parseDevices);
     }
-    
 }
 </script>
