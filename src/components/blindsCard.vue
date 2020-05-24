6198c3117b82d75db6593c0acec623ed351b99ae
Ignacio Mendez <ignmendez@itba.edu.ar>
2020-05-24 14:19:39 -0300

    Blinds front updated, with user-fiendly error handling

--- src/components/blindsCard.vue
+++ src/components/blindsCard.vue
@@ -6,7 +6,7 @@
                 <v-btn color="red lighten-2"  dark  v-on="on" @click="blindsManager">Click Me</v-btn>
             </template>
 
-            <v-card>
+            <v-card min-height="400">
                 <v-container>
                     <v-card-title class="headline blue lighten-4 pa-3" primary-title>
                         Cortina de Nacho
@@ -34,34 +34,44 @@
                                 >CLOSE</v-btn>
                             </v-row>
                             <v-row align="center" justify="center" class="mt-7">
-                                <p>Current State: {{this.currentState}}</p>
+                                <p class="my-2">Current State: {{this.currentState}}</p>
                             </v-row>
                             <v-row align="center" justify="center">
-                                <p>Level of obscurity set: {{this.levelOfObscurity}}</p>
+                                <p class="my-2">Level of obscurity set: {{this.levelOfObscurity}}</p>
                             </v-row>
                             <v-row align="center" justify="center">
-                                <p>Current level: {{this.currentLevel}}</p>
+                                <p class="my-2">Current level: {{this.currentLevel}}</p>
                             </v-row>
-                            <v-row align="center" justify="center">
-                                <v-text-field
+                            <v-row class="mt-8">
+                                <v-slider
+                                    class="mr-2"
+                                    v-model="levelOfObscurity"
                                     v-show="fullyOpenedOrClosed"
-                                    @change="setObscurityLevelToChange" 
-                                    counter
-                                    maxlength="3"
-                                    label="Obscurity Level"
-                                    hint="Number between 1 and 100"
-                                    persistent-hint
-                                ></v-text-field>
-                                <v-btn
-                                    color="blue lighten-1 white--text"
-                                    v-show="fullyOpenedOrClosed"
-                                    :disabled="!readyToChangeObscurityLevel"
-                                    :loading="waitingForSetLevelConfirmation"
-                                    @click="changeObscurityLevel"
-                                >Change Obscurity Level</v-btn>
+                                    thumb-label="always"
+                                    label="Obscurity Level" 
+                                    min="0"
+                                    max="100"
+                                    @change="changeObscurityLevel"
+                                >
+                                    <template v-slot:thumb-label="{ value }">
+                                        <v-icon color="white">{{ showObscurityLevelThumb(value) }}</v-icon>
+                                    </template>
+                                </v-slider>
                             </v-row>
                         </v-container>
                     </v-card-actions>
+
+                    <v-snackbar
+                        :timeout="timeout"
+                        left
+                        bottom
+                        multi-line
+                        v-model="snackbar"
+                        color="error"
+                    >
+                        <strong>{{ errorText }}</strong>
+                        <v-btn @click.native="snackbar = false">Close</v-btn>
+                    </v-snackbar>
 
                 </v-container>
             </v-card>
@@ -78,8 +88,9 @@ export default {
             currentLevel: 0,  // [0-100]
             currentState: "",
 
-            obscurityLevelToChange: 0,
-            readyToChangeObscurityLevel: true,
+            timeout: 6000,    /////
+            errorText: "",    // ERROR HANDLING
+            snackbar: false,  /////
 
             opening: false,
             closing: false,
@@ -97,7 +108,6 @@ export default {
             this.showCard = false;
             clearInterval(this.secondsUpdater);
         },
-    
         blindsManager() {
             const state = '/state';
             this.waitingForCloseConfirmation = true;
@@ -105,33 +115,35 @@ export default {
             this.waitingForSetLevelConfirmation = true; 
             this.axios.get('http://127.0.0.1:8081/api/' + 'devices/' + '809a7de0ee0a2a6b' + state)
             .then( (response) => {
-                this.levelOfObscurity = response.data.result.level;
-                this.currentLevel = response.data.result.currentLevel;
-                this.currentState = response.data.result.status;
-                if(this.currentState === 'closing') {
-                    this.startIntervalTimer();
-                    this.closing = true;
-                    this.opening = false;
-                } else if(this.currentState === 'opening') {
-                    this.startIntervalTimer();
-                    this.closing = false;
-                    this.opening = true;
-                } else if(this.currentState === 'closed') {
-                    this.closing = true;
-                    this.opening = false;
-                    this.fullyOpenedOrClosed = true;
-                } else if(this.currentState === 'opened') {
-                    this.closing = false;
-                    this.opening = true;
-                    this.fullyOpenedOrClosed = true;
-                }
-                this.waitingForCloseConfirmation = false;
-                this.waitingForOpenConfirmation = false;
-                this.waitingForSetLevelConfirmation = false;
-                this.readyToChangeObscurityLevel = false;
+                if(response.data.result.level != "undefined") {
+                    this.levelOfObscurity = response.data.result.level;
+                    this.currentLevel = response.data.result.currentLevel;
+                    this.currentState = response.data.result.status;
+                    if(this.currentState === 'closing') {
+                        this.startIntervalTimer();
+                        this.closing = true;
+                        this.opening = false;
+                    } else if(this.currentState === 'opening') {
+                        this.startIntervalTimer();
+                        this.closing = false;
+                        this.opening = true;
+                    } else if(this.currentState === 'closed') {
+                        this.closing = true;
+                        this.opening = false;
+                        this.fullyOpenedOrClosed = true;
+                    } else if(this.currentState === 'opened') {
+                        this.closing = false;
+                        this.opening = true;
+                        this.fullyOpenedOrClosed = true;
+                    }
+                    this.waitingForCloseConfirmation = false;
+                    this.waitingForOpenConfirmation = false;
+                    this.waitingForSetLevelConfirmation = false;
+                } else
+                    this.throwErrorMessage("Could not load Device state. Try again later.", 0);
             })
             .catch( () => {
-                console.log("No se pudo recuperar el estado al abrir el Popup")
+                this.throwErrorMessage("Could not load Device state. Try again later.", 0);
             })
         },
         startIntervalTimer() {
@@ -139,24 +151,35 @@ export default {
             this.secondsUpdater = window.setInterval( () => {
                 this.axios.get('http://127.0.0.1:8081/api/' + 'devices/' + '809a7de0ee0a2a6b' + '/state')
                 .then( (response) => {
-                    this.levelOfObscurity = response.data.result.level;
-                    this.currentLevel = response.data.result.currentLevel;
-                    this.currentState = response.data.result.status;
+                    if(response.data.result.level != "undefined") {
+                        this.levelOfObscurity = response.data.result.level;
+                        this.currentLevel = response.data.result.currentLevel;
+                        this.currentState = response.data.result.status;
 
-                    if(this.currentState === 'opening')
-                        this.opening = true;
-                    else if(this.currentState === 'closing')
-                        this.closing = true;
-                    
-                    if(this.currentState === 'closed' || this.currentState === 'opened') {
+                        if(this.currentState === 'opening')
+                            this.opening = true;
+                        else if(this.currentState === 'closing')
+                            this.closing = true;
+                        else if(this.currentState === 'closed' || this.currentState === 'opened') {
+                            clearInterval(this.secondsUpdater);
+                            this.fullyOpenedOrClosed = true;
+                            this.getCurrentState();
+                            this.secondsUpdater = 0;
+                        }
+                    } else {
+                        this.throwErrorMessage("Could not load Device state. Try again later.", 0);
                         clearInterval(this.secondsUpdater);
-                        this.fullyOpenedOrClosed = true;
-                        this.getCurrentState();
-                        this.secondsUpdater = 0;
+                        this.waitingForCloseConfirmation = true;
+                        this.waitingForOpenConfirmation = true;
+                        this.waitingForSetLevelConfirmation = true;
                     }
                 })
                 .catch( () => {
-                    console.log("No se pudo recuperar el estado al abrir el Popup")
+                    this.throwErrorMessage("Could not load Device state. Try again later.", 0);
+                    clearInterval(this.secondsUpdater);
+                    this.waitingForCloseConfirmation = true;
+                    this.waitingForOpenConfirmation = true;
+                    this.waitingForSetLevelConfirmation = true;
                 })
             }, 4000);
         },
@@ -164,20 +187,30 @@ export default {
             const state = '/state';
             this.axios.get('http://127.0.0.1:8081/api/' + 'devices/' + '809a7de0ee0a2a6b' + state)
             .then( (response) => {
-                this.levelOfObscurity = response.data.result.level;
-                this.currentLevel = response.data.result.currentLevel;
-                this.currentState = response.data.result.status;
-                if(this.currentState === 'closed') {
-                    this.closing = true;
-                    this.fullyOpenedOrClosed = true;
-                    this.opening = false;
-                } else if(this.currentState === 'opened') {
-                    this.closing = false;
-                    this.fullyOpenedOrClosed = true;
-                    this.opening = true;
+                if(response.data.result.level != "undefined") {
+                    this.levelOfObscurity = response.data.result.level;
+                    this.currentLevel = response.data.result.currentLevel;
+                    this.currentState = response.data.result.status;
+                    if(this.currentState === 'closed') {
+                        this.closing = true;
+                        this.fullyOpenedOrClosed = true;
+                        this.opening = false;
+                    } else if(this.currentState === 'opened') {
+                        this.closing = false;
+                        this.fullyOpenedOrClosed = true;
+                        this.opening = true;
+                    }
+                    this.waitingForSetLevelConfirmation = false;
+                } else {
+                    this.throwErrorMessage("Could not load Device state. Try again later.", 0);
+                    this.waitingForOpenConfirmation = true;
+                    this.waitingForCloseConfirmation = true;
                 }
-                this.waitingForSetLevelConfirmation = false;
-                this.readyToChangeObscurityLevel = false;
+            })
+            .catch( () => {
+                this.throwErrorMessage("Could not load Device state. Try again later.", 0);
+                this.waitingForOpenConfirmation = true;
+                this.waitingForCloseConfirmation = true;
             })
         },
         openBlinds() {
@@ -189,16 +222,17 @@ export default {
                     this.opening = true;
                     this.closing = false;
                     this.currentState = 'opening';
-
                     if(this.secondsUpdater != 0)
                         clearInterval(this.secondsUpdater);
                     this.startIntervalTimer();
-
-                    this.waitingForOpenConfirmation = false;
+                } else {
+                    this.throwErrorMessage("Could not open Blinds. Try again later.", 6000);
                 }
+                this.waitingForOpenConfirmation = false;
             })
             .catch( () => {
-                console.log("No se pudo abrir las blinds");
+                this.throwErrorMessage("Could not open Blinds. Try again later.", 6000);
+                this.waitingForOpenConfirmation = false;
             })
         },
         closeBlinds() {
@@ -210,42 +244,45 @@ export default {
                     this.opening = false;
                     this.closing = true;
                     this.currentState = 'closing';
-
                     if(this.secondsUpdater != 0) {
                         console.log("Estoy terminado el TIMER anterior")
                         clearInterval(this.secondsUpdater);
                     }
                     this.startIntervalTimer();
-
-                    this.waitingForCloseConfirmation = false;
-                }
+                } else
+                    this.throwErrorMessage("Could not close Blinds. Try again later.", 6000);
+                this.waitingForCloseConfirmation = false;
             })
             .catch( () => {
-                console.log("No se pudo cerrar las blinds");
+                this.throwErrorMessage("Could not close Blinds. Try again later.", 6000);
+                this.waitingForCloseConfirmation = false;
             })
         },
-        setObscurityLevelToChange(selectObj) {
-            this.obscurityLevelToChange = selectObj;
-            this.readyToChangeObscurityLevel = true;
+        changeObscurityLevel(selectObj) {
+            const setLevel = '/setLevel';
+            this.axios.put('http://127.0.0.1:8081/api/' + 'devices/' + '809a7de0ee0a2a6b' + setLevel, [selectObj])
+            .then( () => {
+                this.getCurrentState();
+            })
+            .catch( () => {
+                this.throwErrorMessage("Could not change obscurity level. Try again later.", 6000);
+                this.waitingForSetLevelConfirmation = false;
+            })
         },
-        changeObscurityLevel() {
-            if( (this.currentState === 'closing' && this.obscurityLevelToChange <= this.currentLevel) ||
-                 (this.currentState === 'opening' && this.obscurityLevelToChange >= this.currentLevel) ) 
-            {
-                console.log("No se puede cambiar el nivel de obscuridad de esa manera!!!")
-            } else {
-                this.waitingForSetLevelConfirmation = true;
-                const setLevel = '/setLevel';
-                this.axios.put('http://127.0.0.1:8081/api/' + 'devices/' + '809a7de0ee0a2a6b' + setLevel, [this.obscurityLevelToChange])
-                .then( (response) => {
-                    if(response.data.result === this.levelOfObscurity.toString())
-                        this.getCurrentState();
-                    //this.waitingForSetLevelConfirmation = false;
-                })
-                .catch( () => {
-                    console.log("No se pudo cambiar el nivel de obscurity");
-                })
-            }
+        showObscurityLevelThumb(value) {
+            if(value === 0)
+                return "mdi-brightness-1";
+            else if(value > 0 && value <= 50)
+                return "mdi-brightness-2";
+            else if(value > 50 && value < 100)
+                return "mdi-brightness-3";
+            else
+                return "mdi-close";
+        },
+        throwErrorMessage(message, duration) {
+            this.snackbar = true;
+            this.errorText = message;
+            this.timeout = duration;
         }
     }
 }
